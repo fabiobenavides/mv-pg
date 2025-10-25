ad9b13bae53391a0870c69a38c673ea24dae873b
Fabio Benavides <fabio.benavides@gmail.com>
2025-10-24 18:05:27 -0600

    Change hardcoded values by constants

--- backend/vehicle-fee-api/src/VehicleFeeApi/Calculators/VehicleFeeCalculator.cs
+++ backend/vehicle-fee-api/src/VehicleFeeApi/Calculators/VehicleFeeCalculator.cs
@@ -5,6 +5,16 @@ namespace VehicleFeeApi.Calculators
 {
     public class VehicleFeeCalculator : IVehicleFeeCalculator
     {
+        private const decimal MINIMUN_BASE_PRICE = 1m;
+        private const decimal LIMIT_500_BASE_PRICE = 500m;
+        private const decimal ZERO_FEE = 0m;
+        private const decimal LIMIT_500_FEE = 5m;
+        private const decimal LIMIT_1000_BASE_PRICE = 1000m;
+        private const decimal LIMIT_1000_FEE = 10m;
+        private const decimal LIMIT_3000_BASE_PRICE = 3000m;
+        private const decimal LIMIT_3000_FEE = 15m;
+        private const decimal MAXIMUN_FEE = 20m;
+        private const decimal STORAGE_FEE = 100m;
         private readonly ICalculateFee _feeCalculator;
         private readonly decimal _basePrice;
 
@@ -27,24 +37,24 @@ namespace VehicleFeeApi.Calculators
 
         private decimal CalculateAssociationFee()
         {
-            if (_basePrice < 1) {
-                return 0m;
+            if (_basePrice < MINIMUN_BASE_PRICE) {
+                return ZERO_FEE;
             }
-            if (_basePrice <= 500m) {
-                return 5m;
+            if (_basePrice <= LIMIT_500_BASE_PRICE) {
+                return LIMIT_500_FEE;
             }
-            if (_basePrice <= 1000m) {
-                return 10m;
+            if (_basePrice <= LIMIT_1000_BASE_PRICE) {
+                return LIMIT_1000_FEE;
             }
-            if (_basePrice <= 3000m) {
-                return 15m;
+            if (_basePrice <= LIMIT_3000_BASE_PRICE) {
+                return LIMIT_3000_FEE;
             }
-            return 20m;
+            return MAXIMUN_FEE;
         }
 
         private decimal CalculateStorageFee()
         {
-            return 100m;
+            return STORAGE_FEE;
         }
     }
 }
